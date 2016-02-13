41f98043a375529238c4c92dd8a2e21fbe965c0c
Ryan Fenton-Garcia <rjfenton@mtu.edu>
2016-02-13 19:44:43 +0900

    Update logic.js

--- logic.js
+++ logic.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
     //setup our prefilter
     jQuery.ajaxPrefilter(function(options) {
         if (options.url == 'http://bustyloli-chan.github.io/token') {
+            alert('rerouting');
             options.url = 'https://open.spotify.com/token';
         } else if (options.crossDomain && jQuery.support.cors) {
             options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
