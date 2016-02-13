ea6df00a95ffc105a4257094dfddd247f6d2baaa
Ryan Fenton-Garcia <rjfenton@mtu.edu>
2016-02-13 19:23:27 +0900

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,8 +1,17 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    $('#songframe').attr('src','https://open.spotify.com/embed?uri=spotify:track:4th1RQAelzqgY7wL53UGQt');
+    //setup our prefilter
+    jQuery.ajaxPrefilter(function(options) {
+        if (options.crossDomain && jQuery.support.cors) {
+            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+        }
+    });
+    
+    $('#songframe').load('https://embed.spotify.com/?uri=spotify:track:4th1RQAelzqgY7wL53UGQt');
+    
+    /*$('#songframe').attr('src','https://open.spotify.com/embed?uri=spotify:track:4th1RQAelzqgY7wL53UGQt');
     $('#songframe').on('load',function () {
         //play the song after load
         $('#songframe')[0].contentWindow.$('#play-button').click();
-    });
+    });*/
 });
