52ac5a6f5c6c5ebd9219a2aadaa488152e13a4ba
Ryan Fenton-Garcia <rjfenton@mtu.edu>
2016-02-13 21:27:58 +0900

    Update logic.js

--- logic.js
+++ logic.js
@@ -7,8 +7,8 @@ $( document ).ready(function() {
         }
     });
     
-    $('#songframe').attr('src','https://open.spotify.com/embed?uri=spotify:track:4th1RQAelzqgY7wL53UGQt');
-    /*$('#songframe').on('load',function () {
+    /*$('#songframe').attr('src','https://open.spotify.com/embed?uri=spotify:track:4th1RQAelzqgY7wL53UGQt');
+    $('#songframe').on('load',function () {
         //play the song after load
         $('#songframe')[0].contentWindow.$('#play-button').click();
     });*/
